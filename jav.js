9c71cf294da095386da45006c8cef0b40b3e1461
innocentsam217 <usamaiqbal00787@gmail.com>
2024-03-01 19:20:55 +0500

    First Commit PAssword Generstor

---
+++ jav.js
@@ -0,0 +1,42 @@
+
+
+const  passwordBox = document.getElementById("password")
+const lenght = 12;
+
+// const copybtn = document.getElementById("copybtn")
+
+
+const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYX";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number =  "0123456789";
+const symbol = "@#$%^&*()!~{}[]";
+
+const allchars = upperCase + lowerCase + number + symbol;
+
+
+function createPassword () {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length) {
+        password += allchars[Math.floor(Math.random() * allchars.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+// function copypassword () {
+//     copybtn.addEventListener("click",() => {
+//         navigator.clipboard.writeText(passwordBox.value)
+//     })
+
+// }
+
+function copypassword () {
+    passwordBox.select();
+    document.execCommand("copy")
+
+}
